69252ea8db1ea9ec0e164b64df162066ea3ba567
Miguel Coder <syntacticsolutions@gmail.com>
2024-04-08 15:14:50 -0700

    Test

--- ExpressWrapper/index.js
+++ ExpressWrapper/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Setup your express routes
 
-app.use("/__/auth", express.static("auth"));
+app.use('/__/auth', express.static(path.join(__dirname, 'auth')));
 app.use("/__/firebase", express.static("firebase"));
 
 // More routes and middleware can be added here
